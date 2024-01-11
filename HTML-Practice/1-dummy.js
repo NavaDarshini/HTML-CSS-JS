213e1b9812f04b107ae96ec6572d095c8f827fdb
NavaDarshini <alladanavadarshini@gmail.com>
2024-01-11 11:05:57 +0530

    message

---
+++ HTML-Practice/1-dummy.js
@@ -0,0 +1,28 @@
+//first time js
+/*variables*/
+/*var, let and const*/
+/*var till 2015(ES6)*/
+// let p=10;
+// var f=39;
+// const g=20;
+// let p=10;
+// p=59;
+// var f=91;
+// f=933;
+// const g=38;
+// console.log(p,f,g)
+// console.log(type(f))
+// a=792.9136;
+// console.log(a.toFixed(2))
+// console.log(a.toprecision(3))
+// console.log(a.tostring(4))
+// a=389;
+// b=-371;
+// console.log(abs(b))
+// console.log(max(a,b))
+// console.log(min(a,b))
+// let str="the time is 12'0 clock"
+// let sen="the kalam said \"do it"
+// console.log(str)
+// console.log(sen)
+
